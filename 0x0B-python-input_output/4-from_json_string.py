f869c398ffa7e61dd6855e2bd7c755afa061af02
YOUNKAP NINA Duplex <nina.younkap@ccaa.pro>
2023-09-12 23:55:59 -0700

    update Q2 and Q3

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
+"""
+contain the JSON string
+"""
+
 import json
+
+
 def from_json_string(my_str):
-    return(json.loads(my_str))
+    """returns the JSON representation of an object (string)"""
+    return json.loads(my_str)
