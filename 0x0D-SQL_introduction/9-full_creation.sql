89859ee5ebb38f1074c9b1388dff48a041dac60e
YOUNKAP <nina.younkap@ccaa.pro>
2023-10-17 13:07:10 -0400

    update full creation

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -5,7 +5,7 @@ CREATE TABLE IF NOT EXISTS `second_table`(
 	`score` INT
 );
 -- Insert values into the table 'second_tables'
-INSERT INTO `second_table` VALUES(1,"John",10);
-INSERT INTO `second_table` VALUES(2,"Alex",3);
-INSERT INTO `second_table` VALUES(3,"Bob",14);
-INSERT INTO `second_table` VALUES(4,"Goerge",8);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES(1,"John",10);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES(2,"Alex",3);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES(3,"Bob",14);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES(4,"George",8);
