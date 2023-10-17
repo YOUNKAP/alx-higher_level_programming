6c11069684e74d258faea1e9caf35c2c97d136f6
YOUNKAP <nina.younkap@ccaa.pro>
2023-10-17 13:49:33 -0400

    update row

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,5 +1,2 @@
---   lists all records with a score >= 10 in the table "second_table"
-SELECT  `score` , `name` 
-FROM `second_table` 
-WHERE `score` >= 10
-ORDER BY `score` DESC;
+--  creates a table `second_table` and insert values
+SELECT  `score` , `name`  FROM `second_table` ORDER BY `score` DESC;
