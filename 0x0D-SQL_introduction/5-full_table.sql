0b66f34d6e192c3143aa5580b9accd70b666fa6a
YOUNKAP <nina.younkap@ccaa.pro>
2023-10-17 11:54:05 -0400

    full describtion

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,2 +1,3 @@
 -- Print  description of the table "first_table" from db "hbtn_0c_0"
-SHOW COLUMNS FROM first_table IN hbtn_0c_0;
+-- SHOW COLUMNS FROM first_table IN hbtn_0c_0;
+SHOW CREATE TABLE `first_table`;
