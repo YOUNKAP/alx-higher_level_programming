c26fa87b131b8d91219ee8185d3bb3c70b2bd577
YOUNKAP <nina.younkap@ccaa.pro>
2023-11-23 20:59:16 -0500

    update select-states

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -3,13 +3,21 @@
 import MySQLdb
 from sys import argv
 
-# make a connection to the database
-db = MySQLdb.connect(host="localhost", port=3306, \
-	user=argv[1], passwd=argv[2], db=argv[3])
-cur = db.cursor()
-cur.execute("SELECT * FROM states")
-rows = cur.fetchall()
-for i in rows:
-	print(i)
-cur.close()
-db.close()
+# The code should not be executed when imported
+if __name__ == '__main__':
+
+    # make a connection to the database
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3])
+
+    # It gives us the ability to have multiple seperate working environments
+    # through the same connection to the database.
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states")
+
+    rows = cur.fetchall()
+    for i in rows:
+        print(i)
+    # Clean up process
+    cur.close()
+    db.close()
