7390438b7a2bbec068cd27364b112e66be301fd6
YOUNKAP <nina.younkap@ccaa.pro>
2024-01-03 19:07:43 -0500

    update 0-readme

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
-const fs = require('fs');
-const argms = process.argv[2];
 
-fs.readFile(argms, 'utf-8', (err, data) => {
+const fs = require('fs');
+
+fs.readFile(process.argv[2], 'utf8', function (err, data) {
   if (err) {
     console.log(err);
   } else {
-    console.log(data.toString());
+    process.stdout.write(data);
   }
 });
